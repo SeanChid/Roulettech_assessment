8de7833a3f6c24335ee14f6546937320f04fbff0
SeanChid <seanchidester737@gmail.com>
2024-08-20 16:39:18 -0600

    added cors

--- backend/Roulettech_assessment/Roulettech_assessment/settings.py
+++ backend/Roulettech_assessment/Roulettech_assessment/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'roulettech_assess',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Roulettech_assessment.urls'
@@ -69,6 +73,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Roulettech_assessment.wsgi.application'
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
--- backend/Roulettech_assessment/roulettech_assess/views.py
+++ backend/Roulettech_assessment/roulettech_assess/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+@api_view(['GET'])
+def api_overview(request):
+    api_urls = {
+        'List': '/task-list'
+    }
+    return Response(api_urls)
 # Create your views here.
